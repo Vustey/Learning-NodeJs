10e585c116124882fc425e859a0636fad90ca04e
“Vustey” <vutv579@gmail.com>
2024-05-28 17:24:09 +0700

    Using the Node Modules System

--- Module3_UnderstandingTheBasics/app.js
+++ Module3_UnderstandingTheBasics/app.js
@@ -1,11 +1,19 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-});
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url, req.method, req.headers);
+//     // process.exit();
+//     res.setHeader('content-type','text/html');
+//     res.write('<html>');
+//     res.write('<head><title> My First Page</title></head>');
+//     res.write('<body><h1>Hello Tran Thanh Vu</h1></body>');
+//     res.write('</html>');
+//     res.end();
+    
 
-server.listen(3000);
+// });
+
+// server.listen(3000);
 
 // const http = require('http');
 
@@ -17,3 +25,13 @@ server.listen(3000);
 // server.listen(3000, () => {
 //     console.log('Server is listening on port 3000');
 // });
+
+const http = require('http');
+
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
